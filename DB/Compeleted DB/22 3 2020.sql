0bf8c2418efa489eaf8f7c82cf493bde038e00ee
ahmadkhrezaty1 <ahmadkhrezaty1@gmail.com>
2020-03-22 20:02:49 -0700

    Update Database

--- DB/Compeleted DB/22 3 2020.sql
+++ DB/Compeleted DB/22 3 2020.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Feb 04, 2020 at 02:02 AM
+-- Generation Time: Mar 23, 2020 at 04:01 AM
 -- Server version: 10.1.35-MariaDB
 -- PHP Version: 7.1.12
 
@@ -40,8 +40,13 @@ CREATE TABLE `groups` (
 
 INSERT INTO `groups` (`id`, `name`, `description`) VALUES
 (1, 'admin', 'Administrator'),
-(2, 'members', 'General User'),
-(3, 'staff', 'customers');
+(2, 'student', 'Student User'),
+(3, 'staff', 'customers'),
+(4, 'teacher', 'Teacher User'),
+(5, 'accountant', 'Accountant user'),
+(6, 'receptionist', 'Receptionist User'),
+(7, 'librarian', 'Librarian User'),
+(8, 'parents', 'Parents User');
 
 -- --------------------------------------------------------
 
@@ -55,6 +60,16 @@ CREATE TABLE `login_attempts` (
   `login` varchar(100) NOT NULL,
   `time` int(11) UNSIGNED DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `login_attempts`
+--
+
+INSERT INTO `login_attempts` (`id`, `ip_address`, `login`, `time`) VALUES
+(1, '::1', 'admin@admin.com', 1584931277),
+(2, '::1', 'admin@admin.com', 1584932177),
+(3, '::1', 'admin@admin.com', 1584932186),
+(4, '::1', 'admin@admin.com', 1584932190);
 
 -- --------------------------------------------------------
 
@@ -96,19 +111,8 @@ CREATE TABLE `users` (
 --
 
 INSERT INTO `users` (`id`, `ip_address`, `username`, `password`, `email`, `activation_selector`, `activation_code`, `forgotten_password_selector`, `forgotten_password_code`, `forgotten_password_time`, `remember_selector`, `remember_code`, `created_on`, `last_login`, `active`, `first_name`, `last_name`, `company`, `phone`, `city`, `country`, `address`, `dir`, `default_language`, `avatar`, `user_type`) VALUES
-(1, '127.0.0.1', 'admin', '$2y$12$KxhYRSJLjFE6C2KnJ5oio.mWwAVXFR.SaX0taWfC/TD/YEREnMXt2', 'admin@admin.com', NULL, '', NULL, NULL, NULL, '08ed4ff4936ecbd987f739828759cff2e93a4b8a', '$2y$10$rFmIoV2nN7YZydPQq44rHOVeNHV7HUCLZFIXhIFN43d2mDOTih87O', 1268889823, 1571523125, 1, 'ahmad zaher', 'khrezaty', 'ahmad', '0948842976', 'aleppo', 'syria', 'new aleppo', 'ltr', '', NULL, 0),
-(3, '::1', 'ahmadzaher', '$2y$12$eunAl/bwbBMd4FtZrzXb1e11M9EW72h8wefuuOJI9U1SKue2fVfeG', 'ahmad.khrezaty@gmail.com', NULL, NULL, NULL, NULL, NULL, '6ba7d171662be13b308f46ec433bb17cca231e81', '$2y$10$yhmK94OQK705/B0OWiHSDOyK9QjLfuyx23wVd4bJ3980DLvBUEugi', 1565685686, 1580691821, 1, 'Ahmad', 'Khrezaty', 'Ebla Team', '0948842976', 'حلب', 'سورية', 'حلب الجديدة', 'ltr', 'arabic', NULL, 2),
-(4, '::1', 'ahmadkhrezaty', '$2y$12$KxhYRSJLjFE6C2KnJ5oio.mWwAVXFR.SaX0taWfC/TD/YEREnMXt2', 'ahmadkhrezaty@gmail.com', NULL, NULL, NULL, NULL, NULL, 'ab85c7b5a6e6be0144e0b8e5386fc6b4dda91cb9', '$2y$10$AvRC6EK878RW55bpM0122.z49dmXxWcY6obkM3aJzEXJcU/xEtDNq', 1565686139, 1574553039, 1, 'Ahmad Zaher', 'Khrezaty', '', '0948842978', 'Aleppo', 'Syria', 'Halap Aljadidah', 'ltr', '', NULL, 0),
-(5, '::1', 'ahmadkhrezatykh', '$2y$10$FVceQQ25UKA3dk1oNFdP1eMpD5P0FjYAJOqqD/bia.gcXSxIlWmeW', 'ahmadkhrezatykh@gmail.com', NULL, NULL, NULL, NULL, NULL, NULL, NULL, 1566386192, 1570627269, 1, 'ahmad', 'adsdas', 'adas', '', '', '', '', 'rtl', '', '', 0),
-(6, '::1', 'ahmadkhrezaty', '$2y$10$dEW9afWGpNG5CqIqng6gSuh6zqnjpK8L5e0Zs08pab.p9rcGLvl1i', 'ahmadkhreza@gmail.com', NULL, NULL, NULL, NULL, NULL, NULL, NULL, 1566386216, NULL, 1, 'asddas', 'fdsf', 'cxzzxc', '', '', '', '', '', '', '', 0),
-(7, '::1', 'ahmadkhrezaty1@gmail.com', '$2y$10$NCkno4JxbZ9p/2WUaYYzx.qnNIMedYu9SOhDDmGE.SMLx4/EDWAMC', 'ahmadkhrezaty1@gmail.com', NULL, NULL, NULL, NULL, NULL, NULL, NULL, 1566386252, NULL, 1, 'sadas', 'dasdas', 'xzczxc', '0921312313213', NULL, NULL, NULL, '', '', '', 0),
-(8, '::1', 'ahmadkhrezatykhss@gmail.com', '$2y$10$/4eqdWuP8xRO4xMRrFlUuO632L4gDylkZMyah9BYmCeqVY6m/8j5y', 'ahmadkhrezatykhss@gmail.com', 'f62f280ddfc2028d2104', '$2y$10$cSPVR35Q8PqN4w4jFTsti.1qoF3hK0mKV8FeeYHp2ExAyveMyls2e', NULL, NULL, NULL, NULL, NULL, 1566386275, NULL, 0, 'vxcvxcv', 'bxvdsf', 'fdsfhbfcvb', '13213123123', NULL, NULL, NULL, '', '', '', 0),
-(9, '::1', 'ahmadkhrezaty12@gmail.com', '$2y$10$9mL0tcHZhQvRRY6p6A2.s.ed0bV4uamrCUqRd3Hs5wyELecH54Dvi', 'ahmadkhrezaty12@gmail.com', NULL, NULL, NULL, NULL, NULL, NULL, NULL, 1566386295, NULL, 1, 'cvdsfg', 'xbcvdfgv', 'sdfwetwer', '3123123', NULL, NULL, NULL, '', '', '', 0),
-(10, '::1', 'ahmadkhrezaty13213@gmail.com', '$2y$10$OpYv1VF7VzSM.05W0RI7B.slbceQ9.BUM094W9wlDS9EI60Vo72gO', 'ahmadkhrezaty13213@gmail.com', NULL, NULL, NULL, NULL, NULL, NULL, NULL, 1566386332, NULL, 1, 'zxcfhxcv', 'qe\\zasd', 'xzczxc', '09123123321', NULL, NULL, NULL, '', '', '', 0),
-(11, '::1', 'ahmadkhrezaty1342@gmail.com', '$2y$10$nvVmmnB.E0FsG/I1qm6p3eKFCHDFfgMl9JhXS1Hfr.UUxUp8xmqG6', 'ahmadkhrezaty1342@gmail.com', '6c435aba4bcf87bfc397', '$2y$10$gepBp7dOE1gNPTDEYFVUKukteekPMGVfx5er2/RQ8oUNfwfnkpHcS', NULL, NULL, NULL, NULL, NULL, 1566386363, NULL, 0, 'dasdasd', 'dfsdvczx', 'vxcvfdg', '09123123', NULL, NULL, NULL, '', '', '', 0),
-(12, '::1', 'ahmadkhrezaty1123123@gmail.com', '$2y$10$Mk85sQ3YRFbt0x3Ud2Z7uOsujoxkdEeCTCB8AlzTRgUOTCtDyDH/q', 'ahmadkhrezaty1123123@gmail.com', '0c07a338e8a8f6e03a74', '$2y$10$Da2bQWqFME.q4YKJZSPj7u7.jbpP3XTV1Ke4p.wC8s/Wf2MwAT49m', NULL, NULL, NULL, NULL, NULL, 1566386399, NULL, 0, 'أحمد زاهر', 'خريزاتي', 'sadsadasd', '0944543666', '', '', '', '', '', '', 0),
-(13, '::1', 'ahmad@gmail.com', '$2y$10$VVv82mYMyoA/9HJxut8B2entHVQHpfKXXp9fqdvAjK1mxCzCShlCm', 'ahmad@gmail.com', NULL, NULL, NULL, NULL, NULL, NULL, NULL, 1570581311, NULL, 1, 'عبد الجواد', 'نجار', 'dasdas', '', '', '', '', '', '', '', 0),
-(14, '::1', 'jamal@gmail.com', '$2y$10$K0HrryRxFAfNOGQOHwOf8u1e9deZoaLAIObJf/uWQSIOCv4rGGcbe', 'jamal@gmail.com', '593134b6f8c1ab1688f4', '$2y$10$g7fBdYbcHKC47S2ezX5JF.6zZbmLLW5Gg/7UnH5sF8gqzWySFTfXS', NULL, NULL, NULL, NULL, NULL, 1570581351, NULL, 0, 'mohammad', 'jamal', 'jamal company', '0944543666', NULL, NULL, NULL, '', '', '', 0);
+(3, '::1', 'admin', '$2y$12$.ioB./sMhPHEGodsNHIg9eb9sXx8jbSCKEjrczjxUKOwgbmXBHNWy', 'admin@admin', NULL, NULL, NULL, NULL, NULL, NULL, NULL, 1565685686, 1584932194, 1, 'Ahmad', 'Khrezaty', 'Ebla Team', '0948842976', 'حلب', 'سورية', 'حلب الجديدة', 'ltr', 'arabic', NULL, 2),
+(13, '::1', 'ahmad@gmail.com', '$2y$10$VVv82mYMyoA/9HJxut8B2entHVQHpfKXXp9fqdvAjK1mxCzCShlCm', 'ahmad@gmail.com', NULL, NULL, NULL, NULL, NULL, NULL, NULL, 1570581311, NULL, 1, 'عبد الجواد', 'نجار', 'dasdas', '', '', '', '', '', '', '', 0);
 
 -- --------------------------------------------------------
 
@@ -127,22 +131,9 @@ CREATE TABLE `users_groups` (
 --
 
 INSERT INTO `users_groups` (`id`, `user_id`, `group_id`) VALUES
-(99, 1, 1),
-(100, 1, 2),
-(28, 3, 1),
-(30, 3, 3),
-(14, 4, 1),
-(87, 4, 3),
-(17, 5, 2),
-(18, 6, 2),
-(19, 7, 2),
-(20, 8, 2),
-(21, 9, 2),
-(22, 10, 2),
-(23, 11, 2),
-(24, 12, 2),
-(25, 13, 2),
-(26, 14, 2);
+(109, 3, 1),
+(108, 3, 2),
+(25, 13, 2);
 
 --
 -- Indexes for dumped tables
@@ -187,13 +178,13 @@ ALTER TABLE `users_groups`
 -- AUTO_INCREMENT for table `groups`
 --
 ALTER TABLE `groups`
-  MODIFY `id` mediumint(8) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id` mediumint(8) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 
 --
 -- AUTO_INCREMENT for table `login_attempts`
 --
 ALTER TABLE `login_attempts`
-  MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- AUTO_INCREMENT for table `users`
@@ -205,7 +196,7 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `users_groups`
 --
 ALTER TABLE `users_groups`
-  MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=101;
+  MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=110;
 
 --
 -- Constraints for dumped tables
